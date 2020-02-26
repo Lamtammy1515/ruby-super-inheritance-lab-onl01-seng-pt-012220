c57e48f024ceaf6689295faedc757882227783ef
Tammy Lam <lamtammy1515@gmail.com>
2020-02-26 04:01:22 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,13 +1,5 @@
 class Chatty_student < Student
-  def hello 
-    super 
-    puts "Hey there! I'm so excited to learn stuff."
-  end 
-  
-  def raise_hand 
-    super
-    puts "Pick me!"
-  end 
+ 
   
 end 
   
