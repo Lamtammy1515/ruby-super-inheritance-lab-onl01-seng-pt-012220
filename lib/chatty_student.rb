9acf8dc406f487a9009fd0aa942cdf3ae5ec11cf
Tammy Lam <lamtammy1515@gmail.com>
2020-02-26 04:01:56 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,5 +1,13 @@
-class Chatty_student < Student
- 
+class ChattyStudent < Student
+  def hello 
+    super 
+    puts "Hey there! I'm so excited to learn stuff."
+  end 
+  
+  def raise_hand 
+    super
+    puts "Pick me!"
+  end 
   
 end 
   
